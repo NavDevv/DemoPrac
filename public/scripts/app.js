e183788b9987acb63f1f1baff57c26d073aa1097
NavDevv <navjot200284@gmail.com>
2023-07-25 14:16:54 -0500

    ImportAndExportFeature

---
+++ public/scripts/app.js
@@ -0,0 +1,7 @@
+// importing the key from the util.js with the default method 
+import key from "./util.js"
+console.log(key);
+
+// importing from the util.js with the data and name of the file 
+import { NewKey } from "./util.js";
+console.log(NewKey);
---
+++ public/scripts/util.js
@@ -0,0 +1,3 @@
+export default "swiji242i323k"
+// learning the export and import feature 
+export let NewKey = "3232131ffsfsd";
