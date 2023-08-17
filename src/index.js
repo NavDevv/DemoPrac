35d7f4f96279475addca0eacf2eb115c9578c013
NavDevv <navjot200284@gmail.com>
2023-08-16 21:56:07 -0500

    learningReact

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Importing some object from the library which is downloaded locally 
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
