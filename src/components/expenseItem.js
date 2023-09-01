dccdab3ae39c91ec0482cb99cf78812a9a35c206
NavDevv <navjot200284@gmail.com>
2023-08-31 21:57:56 -0500

    HooksAndevents

--- src/components/expenseItem.js
+++ src/components/expenseItem.js
@@ -1,16 +1,27 @@
 import './expenseItem.css';
 import ExpenseDate from './expenseDate'
 import Card from './Card';
+import React, { useState } from 'react';
 // Passing 1 prop and will be used to access attributes from the components
 // We can also use destructing in the parameter section {date, title, amount            }
 function ExpenseItem(data) {
+
+    //  Array destructing
+
+    const [title, setTitle] = useState(data.title);
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+    };
+
     return (
-        <Card className='expense-item'>
+        <Card className='expense-item' >
             <ExpenseDate date={data.date} ></ExpenseDate>
             <div className='expense-item__description'>
-                <h2>{data.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${data.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
