9427cef962dd447b8c68d9f9fa50cbb02cc20cf1
NavDevv <navjot200284@gmail.com>
2023-09-16 19:30:08 -0500

    Changes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,74 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
 function App() {
-  return <h1>Hello React!</h1>
+  
+  return <div>
+     <h1>Hello React!!!!</h1> 
+     <Pizza></Pizza>
+     <Pizza></Pizza>
+  </div>
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+function Pizza(){
+   return (
+    <div>
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci"></img>
+    </div>
+   )
+}
 
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<React.StrictMode><App /></React.StrictMode>)
+
+// To render the projects that used old react versions use this ->
+// React.render(<App />, document.getElementById("root"))
