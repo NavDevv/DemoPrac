c86ea1257fa4f08f30e5205070b746333db07cbe
NavDevv <navjot200284@gmail.com>
2023-09-19 11:46:46 -0500

    JustACard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar></Avatar>
+      <div className="data">
+        <Intro></Intro>
+        <SkillList></SkillList>
+      </div>
+    </div>
+  );
+}
+function Avatar() {
+  return (
+    <div>
+      <img src="#" alt="Photos will be update Soon"></img>
+    </div>
+  );
+}
+function Intro() {
+  return (
+    <div>
+      <h1>Navjot Singh</h1>
+      <p>
+        I am training to be Web Developer. I used to be backend developer and
+        made some desktop applications and now shifted to the front-end
+        programming. I love to play games and coding now
+      </p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skills name="React" color="red" />
+      <Skills name="c#" color="darkgreen" />
+      <Skills name="HTML" color="Yellow" />
+      <Skills name="CSS" color="lightblue" />
     </div>
   );
 }
 
+function Skills(prop) {
+  return (
+    <div className="skill" style={{ backgroundColor: prop.color }}>
+      <span>{prop.name}</span>
+    </div>
+  );
+}
 export default App;
