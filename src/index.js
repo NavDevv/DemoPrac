cd9cd5036410961c4620ffb34005cd032852ec7d
NavDevv <navjot200284@gmail.com>
2023-09-22 20:32:23 -0500

    Fragments

--- src/index.js
+++ src/index.js
@@ -74,12 +74,19 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizza > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+
+      {numPizza < 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from out stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on the menu. Please come back later</p>
       )}
@@ -101,16 +108,16 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
   //if (props.pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.name}></img>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -144,10 +151,10 @@ function Footer() {
   //return React.createElement('footer', null, `We're Currentluy Open`)
 }
 
-function Order(props) {
+function Order({ cHours }) {
   return (
     <div className="order">
-      <p>We're open until {props.cHours}:00. Come visit us or Order online.</p>
+      <p>We're open until {cHours}:00. Come visit us or Order online.</p>
       <button className="btn">Order</button>
     </div>
   );
